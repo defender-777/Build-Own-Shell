81aed688ca10297693f6a8b9c81524c660eb065a
Gagan R <gagan.r.2005@gmail.com>
2025-11-06 19:15:12 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,7 +4,16 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
-    pass
+    #sys.stdout.flush() #Ensure the "$" appears before input 
+
+    #Read user command from input 
+    command = input()
+
+    #Print the 'command not found' message
+    print(f"{command}: command not found")
+
+
+
 
 
 if __name__ == "__main__":
